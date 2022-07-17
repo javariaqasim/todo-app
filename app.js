308d58475418ed98f34b89378028bf7db3a64c08
javaria qasim <javariaqasim77@gmail.com>
2022-07-17 20:13:17 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,43 @@
+ var rightNow = new Date();
+ document.getElementById("date").innerHTML=rightNow
+
+
+var list =document.getElementById("list");
+
+function addTodo(){
+    var todo_item = document.getElementById("todo-item");
+
+    var li =document.createElement('li');
+    var liText=document.createTextNode(todo_item.value)
+    li.appendChild(liText)
+
+    var delBtn = document.createElement("button")
+    var delText = document.createTextNode("DELETE")
+    delBtn.setAttribute("class","btn")
+    delBtn.setAttribute("onClick","deleteItem(this)")
+    delBtn.appendChild(delText)
+
+    var editBtn = document.createElement("button")
+    var editText = document.createTextNode("EDIT")
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("class","btn")
+    editBtn.setAttribute("onClick" , "editItem(this)")
+
+    li.appendChild(delBtn)
+    li.appendChild(editBtn)
+    list.appendChild(li)
+    todo_item.value=""
+    console.log(li)
+}
+function deleteItem(e){
+    e.parentNode.remove()
+}
+function editItem(e){
+    var val = prompt("Enter update value ",e.parentNode.firstChild.nodeValue)
+    e.parentNode.firstChild.nodeValue=val;
+}
+function deleteAll(){
+    list.innerHTML=""
+}
+
+
