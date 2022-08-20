fedafc4776dcd91261bc71bfc9a42ab1f99e8052
javaria qasim <javariaqasim77@gmail.com>
2022-08-20 21:01:48 +0500

    work complete

--- app.js
+++ app.js
@@ -1,44 +1,53 @@
- var rightNow = new Date();
- document.getElementById("date").innerHTML=rightNow
+var rightNow = new Date();
+document.getElementById("date").innerHTML=rightNow
 
 
-var list =document.getElementById("list");
+var list = document.getElementById('todo-list') 
+function addlist(){
 
-function addTodo(){
-    var todo_item = document.getElementById("todo-item");
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("list-item").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    li.setAttribute('class', 'btnlist')
+    if (inputValue === '') {
+      alert("Fill empty space ");
+    } else {
+      document.getElementById("todo-list").appendChild(li);
+    }
+    document.getElementById("list-item").value = "";
+    
 
-    var li =document.createElement('li');
-    li.setAttribute('class', 'li')
-    var liText=document.createTextNode(todo_item.value)
-    li.appendChild(liText)
 
-    var delBtn = document.createElement("button")
-    var delText = document.createTextNode("DELETE")
-    delBtn.setAttribute("class","btn")
-    delBtn.setAttribute("onClick","deleteItem(this)")
-    delBtn.appendChild(delText)
+    var editbtn = document.createElement('button')
+    var editnode = document.createTextNode('Edit')
+    editbtn.appendChild(editnode)
+    editbtn.setAttribute('class', 'editbtn')
+    editbtn.setAttribute('onclick' , 'editItem(this)')
+    li.appendChild(editbtn)
 
-    var editBtn = document.createElement("button")
-    var editText = document.createTextNode("EDIT")
-    editBtn.appendChild(editText)
-    editBtn.setAttribute("class","btn")
-    editBtn.setAttribute("onClick" , "editItem(this)")
+ 
 
-    li.appendChild(delBtn)
-    li.appendChild(editBtn)
-    list.appendChild(li)
-    todo_item.value=""
-    console.log(li)
+    var delbtn = document.createElement('button')
+    var btnnode = document.createTextNode('Delete')
+    delbtn.appendChild(btnnode)
+    delbtn.setAttribute('class', 'delbtn')
+    delbtn.setAttribute('onclick','deleteitem(this)')
+    li.appendChild(delbtn) 
+
+
 }
-function deleteItem(e){
+
+function editItem(e){
+    var v = e.parentNode.firstChild.nodeValue
+    var edit = prompt("Enter updated value ", e.parentNode.firstChild.nodeValue )
+    e.parentNode.firstChild.nodeValue = edit
+}
+
+function deleteitem(e){
     e.parentNode.remove()
 }
-function editItem(e){
-    var val = prompt("Enter update value ",e.parentNode.firstChild.nodeValue)
-    e.parentNode.firstChild.nodeValue=val;
-}
-function deleteAll(){
-    list.innerHTML=""
-}
 
-
+function alldel(){
+    list.innerHTML = ""
+}
